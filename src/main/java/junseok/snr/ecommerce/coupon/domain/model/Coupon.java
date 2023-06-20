fd6c6284229da4d0dcc1f992d691a4449dae8b3d
ixtears23 <ixtears23@icloud.com>
2023-06-20 20:29:20 +0900

    [feature] 쿠폰 도메인 추가 (#6)

---
+++ src/main/java/junseok/snr/ecommerce/coupon/domain/model/Coupon.java
@@ -0,0 +1,29 @@
+package junseok.snr.ecommerce.coupon.domain.model;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+
+
+@Entity
+public class Coupon {
+    @Id @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    private Long userId;
+
+    protected Coupon() {
+    }
+
+    public Coupon(Long id) {
+        this.id = id;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public Long getUserId() {
+        return userId;
+    }
+}
