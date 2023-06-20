ffc7c52a2408d9cf0d30cebef69526ece2aded87
ixtears23 <ixtears23@icloud.com>
2023-06-20 20:36:41 +0900

    [feature] 쿠폰 Repository 에 사용 메서드 추가 (#6)

--- src/main/java/junseok/snr/ecommerce/coupon/domain/repository/CouponRepository.java
+++ src/main/java/junseok/snr/ecommerce/coupon/domain/repository/CouponRepository.java
@@ -1,4 +1,8 @@
 package junseok.snr.ecommerce.coupon.domain.repository;
 
+import junseok.snr.ecommerce.coupon.domain.model.Coupon;
+
 public interface CouponRepository {
+    Coupon save(Coupon coupon);
+    long count();
 }
