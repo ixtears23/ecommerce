0c266262624c2f902a4b7f22788425b7460267d3
ixtears23 <ixtears23@icloud.com>
2023-06-27 23:21:13 +0900

    [feature] 쿠폰 생성 KafkaConsumer 코드 추가 (close #13)

---
+++ src/main/java/junseok/snr/ecommerce/coupon/infrastructure/consumer/CouponCreatedConsumer.java
@@ -0,0 +1,15 @@
+package junseok.snr.ecommerce.coupon.infrastructure.consumer;
+
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.kafka.annotation.KafkaListener;
+import org.springframework.stereotype.Component;
+
+@Slf4j
+@Component
+public class CouponCreatedConsumer {
+
+    @KafkaListener(topics = "coupon_create", groupId = "group_1")
+    public void listener(Long userId) {
+        log.info(">>>>> topic : coupon_create, userId : {}", userId);
+    }
+}
